19b9db4acf6a19d033afaefee9c680b783be499a
EmbarckleAarthi <aarthi.m@embarckle.com>
2023-07-12 12:22:42 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from "react";
+import Axios from "axios";
+import { Card, CardContent, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
+
+  const[list, setList] = useState([]);
+  const[page, setPage] = useState(1);
+
+  useEffect(()=>{
+    Axios.get('https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON')
+    .then(response=>setList(response.data))
+  },[])
+
+  const selectPageHandler = (selectedPage) => {
+    if(
+      selectedPage >=1 &&
+      selectedPage <= list.length / 4 &&
+      selectedPage !== page
+    )
+    setPage(selectedPage)
+  }
+
+  
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <div className="card-container">
+    {
+    list.slice(page * 10 - 10, page * 10).map(movie=>(
+
+      <Card key={movie.Title} className="movie-card"
+      style={{
+          width: 250,height: 350,
+          backgroundColor: "black",
+          margin: "0.5rem",
+          }}
+      >
+
+      
+        <CardContent className="card-content">
+          <Typography variant="h5" className="additional-text">
+            {movie.Title}
+          </Typography>
+
+          <Typography variant="body2" className="additional-text">
+          {movie.Rated}
+          </Typography>
+
+        <Typography variant="body2">
+         {movie.Images && <img src={movie.Images} alt={movie.Title} className="movie-image"/>}
+        </Typography>
+
+         
+      </CardContent>
+     
+      </Card>
+    )
+   
+    )}
+ 
+    {      
+       list.length > 0 && <div className="pagination">
+        <span  onClick= {() => selectPageHandler(page - 1)}>  previous </span>
+
+        {[...Array(list.length / 8)].map((_,i) => {
+            return <span 
+           
+            onClick= {() => selectPageHandler(i+ 1)}key={i}>
+              { i+ 1}</span>
+
+          })}
+       
+        <span  onClick= {() => selectPageHandler(page+ 1)}> Next </span>
+       </div>
+      
+    }
+  </div>
+
+ );
+ 
+ 
 }
 
 export default App;
